acfdb92e8977249f68c8d1586546a68f7f4d25e7
Otabek Tursunov <otabecktursunov@gmail.com>
2024-01-16 21:43:12 +0500

    first commit: created

---
+++ mainApp/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
---
+++ mainApp/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+from rest_framework.generics import *
+
+
+class Get_Token(TokenObtainPairView):
+    serializer_class = TokenObtainPairView.serializer_class
+
+
+class CustomUserListCreateAPIView(ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def index(request):
+    if request.method == 'GET':
+        return Response("Hello World")
